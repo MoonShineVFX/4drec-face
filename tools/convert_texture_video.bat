3d4be83ddaddc0c476844b10c57af71580dbe439
Elisha Hung <mrhchief@gmail.com>
2022-09-09 16:33:31 +0800

    feat: Move metadata to start

--- tools/convert_texture_video.bat
+++ tools/convert_texture_video.bat
@@ -1,1 +1,1 @@
-ffmpeg -r 30 -start_number 0 -i .\texture\bridge_curse_2-jiachi_2_%04d.jpg -i audio.wav -vf scale=2048:-1 texture.mp4
+ffmpeg -r 30 -start_number 0 -i .\texture\bridge_curse_2-jiachi_2_%04d.jpg -i audio.wav -vf scale=2048:-1 -movflags +faststart texture.mp4
