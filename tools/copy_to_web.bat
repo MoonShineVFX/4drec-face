016b003f09f9b8d97d9a0d867c70a78b63c9edba
eli.hung <mrhchief@gmail.com>
2023-06-05 20:47:09 +0800

    feat(export): fix frameCount not working

--- tools/copy_to_web.bat
+++ tools/copy_to_web.bat
@@ -1,5 +1,4 @@
 @ECHO OFF
-setlocal enabledelayedexpansion
 
 REM Check if any folders are dragged onto the script
 if "%~1"=="" (
@@ -8,47 +7,59 @@ if "%~1"=="" (
     exit /b 1
 )
 
-REM Loop through each dragged folder
+REM Main
+setlocal enabledelayedexpansion
 for %%A in (%*) do (
-    REM Main
-    set "gltf_folder=%%A\gltf"
+    set "source_folder=%%A"
     set "web_folder=%%A\..\..\web\%%~nxA"
-    set "frameCount=0"
 
     @ECHO ^>^> Convert %%A
-
-    REM Get frame count
-    for /r "%%A\gltf" %%f in (*) do (
-        set /a frameCount+=1
-    )
-    set /a frameCount-=1
 
     REM Create web_folder if it doesn't exist
     if not exist "!web_folder!" (
         mkdir "!web_folder!"
     )
 
-    @ECHO ^>^> Create metadata.yaml
-    echo.
-    (
-      echo ^{
-      echo   "hires": true,
-      echo   "endFrame": !frameCount!,
-      echo   "meshFrameOffset": -1,
-      echo   "modelPositionOffset": [0, 0.05, 0]
-      echo ^}
-    ) > "!web_folder!\metadata.json"
-
-    @ECHO ^>^> Copy hires
-    robocopy "%%A\gltf" "!web_folder!\hires" /E /MT /NP /NDL /NJH /NJS /BYTES /XJ /W:1 /ETA
-
-    @ECHO ^>^> Copy mesh
-    robocopy "%%A\gltf_mini_drc" "!web_folder!\mesh" /E /MT /NP /NDL /NJH /NJS /BYTES /XJ /W:1 /ETA
-
-    @ECHO ^>^> Copy texture.mp4
-    copy "%%A\texture.mp4" "!web_folder!\texture.mp4" > nul
+    REM Call a subroutine to avoid issues with variable expansion
+    call :ProcessFolder "!source_folder!" "!web_folder!"
 )
-
 endlocal
+
 @ECHO ^>^> Done
 pause
+exit /b
+
+:ProcessFolder
+setlocal enabledelayedexpansion
+set "source_folder=%~1"
+set "web_folder=%~2"
+set "frameCount=0"
+
+REM Get frame count
+for /r "%source_folder%\gltf" %%f in (*) do (
+    set /a frameCount+=1
+)
+set /a frameCount-=1
+
+@ECHO ^>^> Create metadata.yaml
+echo.
+(
+  echo ^{
+  echo   "hires": true,
+  echo   "endFrame": !frameCount!,
+  echo   "meshFrameOffset": -1,
+  echo   "modelPositionOffset": [0, 0.05, 0]
+  echo ^}
+) > "%web_folder%\metadata.json"
+
+@ECHO ^>^> Copy hires
+robocopy "%source_folder%\gltf" "%web_folder%\hires" /E /MT /NP /NDL /NJH /NJS /BYTES /XJ /W:1 /ETA
+
+@ECHO ^>^> Copy mesh
+robocopy "%source_folder%\gltf_mini_drc" "%web_folder%\mesh" /E /MT /NP /NDL /NJH /NJS /BYTES /XJ /W:1 /ETA
+
+@ECHO ^>^> Copy texture.mp4
+copy "%source_folder%\texture.mp4" "%web_folder%\texture.mp4" > nul
+
+endlocal
+exit /b
